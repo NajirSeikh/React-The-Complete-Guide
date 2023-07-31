ec6780677dc0232f9af8fd83e84d13df61a1e70d
NajirSeikh <ns.soft.97@gmail.com>
2023-07-31 08:07:26 +0530

    Writing More Complex JSX Code

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div>
+      <div>July 31st 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
