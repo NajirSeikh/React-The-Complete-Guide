66b80d93ccc9a007e7fecb734414a3d11043d749
NajirSeikh <ns.soft.97@gmail.com>
2023-07-31 09:16:05 +0530

    Splitting Components into Multiple Componts

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -35,12 +35,12 @@ function App() {
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>{' '}
+      ></ExpenseItem>
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>{' '}
+      ></ExpenseItem>
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
