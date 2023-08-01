368edee6f0724ace58384ad8bcd8bcb9fd619906
NajirSeikh <ns.soft.97@gmail.com>
2023-08-01 09:16:50 +0530

    Organizing Component Files

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 import './Expenses.css';
 
 function Expenses(props) {
