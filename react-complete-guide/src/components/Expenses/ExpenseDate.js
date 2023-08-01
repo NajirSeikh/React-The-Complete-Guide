f900a858bb5a89a497ba6160a4d159e54dd5d9eb
NajirSeikh <ns.soft.97@gmail.com>
2023-08-01 09:22:43 +0530

    An Alternative Function Syntax

--- react-complete-guide/src/components/Expenses/ExpenseDate.js
+++ react-complete-guide/src/components/Expenses/ExpenseDate.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './ExpenseDate.css';
 
-function ExpenseDate(props) {
+const ExpenseDate = props => {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
@@ -14,6 +14,6 @@ function ExpenseDate(props) {
       <div className="expense-date__day">{day}</div>
     </div>
   );
-}
+};
 
 export default ExpenseDate;
--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -4,7 +4,7 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = props => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -14,6 +14,6 @@ function ExpenseItem(props) {
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
