cfceb9ef87e4afc2e32b2ba683b3f46010ad377b
NajirSeikh <ns.soft.97@gmail.com>
2023-08-09 22:42:28 +0530

    Adding Validation & Resetting Logic

--- 08 - Time to Practice A Complete Practice Project/src/components/Users/AddUser.js
+++ 08 - Time to Practice A Complete Practice Project/src/components/Users/AddUser.js
@@ -9,7 +9,15 @@ const AddUser = props => {
 
   const addUserHandler = event => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = event => {
@@ -27,11 +35,16 @@ const AddUser = props => {
         <input
           id="username"
           type="text"
+          value={enteredUsername}
           onChange={usernameChangeHandler}
-          name="username"
         />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
 
         <Button type="submit">Add User</Button>
       </form>
