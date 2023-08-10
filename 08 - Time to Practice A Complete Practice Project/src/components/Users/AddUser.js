5273c556cc6445b09f58d225fffbea9a407e0e7f
NajirSeikh <ns.soft.97@gmail.com>
2023-08-10 09:02:55 +0530

    Adding The "ErrorModal" Component

--- 08 - Time to Practice A Complete Practice Project/src/components/Users/AddUser.js
+++ 08 - Time to Practice A Complete Practice Project/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = props => {
@@ -30,26 +31,30 @@ const AddUser = props => {
   };
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
-        />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        />
+    <div>
+      <ErrorModal title="An error occoured!" message="Something went wrong!" />
 
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
